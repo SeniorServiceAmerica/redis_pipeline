4e3f0e77434ab78bff6d0e2ca70e60b2a26666e7
Ian Whitney <ian@ianwhitney.com>
2012-08-24 14:00:02 -0500

    Namespacing the generator

--- lib/generators/redis_pipeline_generator.rb
+++
@@ -1,7 +0,0 @@
-class RedisPipelineGenerator < Rails::Generators::Base
-  source_root File.expand_path("../../templates", __FILE__)
-
-  def copy_initializer_file
-    copy_file "redis_pipeline.example.yml", "config/redis_pipeline.yml"
-  end
-end
